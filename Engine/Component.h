4a3b9b065d29b3657255207b696dced9ae178a5d
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-07-25 16:36:44 +0900

    Modify : Component header
    CRTP 다형성 패턴을 사용해보기 위해 CRTP패턴으로 변경합니다.

--- Engine/Component.h
+++ Engine/Component.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "IComponent.h"
 #include "FamilyTypeId.h"
+#include "EventListener.h"
 
 template <class T>
 class Component :
-    public IComponent
+    public IComponent, public EventListener
 {
 public:
     static const ComponentTypeId COMPONENT_TYPE_ID;
@@ -14,13 +15,19 @@ public:
 
 	ComponentTypeId GetComponentTypeId() const { return COMPONENT_TYPE_ID; }
 
-	virtual void OnInitialize() {}
-	virtual void OnBegin() {}
-	virtual void OnFixedUpdate() {}
-	virtual void OnUpdate(float dt) {}
-	virtual void OnEnd() {}
-	virtual void OnDestroy() {}
+	virtual void OnInitialize()		final { static_cast<T*>(this)->Initialize(); }
+	virtual void OnBegin()			final { static_cast<T*>(this)->Begin(); }
+	virtual void OnFixedUpdate()	final { static_cast<T*>(this)->FixedUpdate(); }
+	virtual void OnUpdate(float dt) final { static_cast<T*>(this)->Update(dt); }
+	virtual void OnEnd()			final { static_cast<T*>(this)->End(); }
+	virtual void OnDestroy()		final { static_cast<T*>(this)->Destroy(); }
 
+	void Initialize()		{}
+	void Begin()			{}
+	void FixedUpdate()		{}
+	void Update(float dt)	{}
+	void End()				{}
+	void Destroy()			{}
 };
 
 template <class T>
