21423491dddf0cb8c4e45d6050f1231a827cc685
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-07-26 16:14:24 +0900

    Modify : sprite component
    기존 sprite component 에서 하던 transform 계산을 transform component에서 수행하게 변경합니다.

--- Engine/Sprite.cpp
+++ Engine/Sprite.cpp
@@ -11,13 +11,9 @@ void Sprite::Update(float dt)
 void Sprite::Render(ID2D1HwndRenderTarget* target)
 {
 	Transform* tf = componentManager->GetComponent<Transform>(_owner);
-	D2D1::Matrix3x2F transform =
-		D2D1::Matrix3x2F::Scale(tf->_scale.x, tf->_scale.y, D2D1::Point2F(_size.x / 2.0f, _size.y / 2.0f)) *
-		D2D1::Matrix3x2F::Rotation(tf->_rotation.z, D2D1::Point2F(_size.x / 2.0f, _size.y / 2.0f)) *
-		D2D1::Matrix3x2F::Translation(tf->_worldPosition.x, tf->_worldPosition.x)
-		;
+	
 	// TODO: Add Camera and mul inverse matrix to transform
-	target->SetTransform(transform);
+	target->SetTransform(tf->_worldTransform);
 
 	SpriteIdSet spSet = ResourceSystem::GetInstance()->GetSpriteSet(_spriteId)->GetSpriteIdSet();
 	auto bitmap = ResourceSystem::GetInstance()->GetSprite(spSet[_startAnimationIdx]);
